dc2f56b5709ddbf9030c16ff1265a3d049cc1e95
Gavin Mogan <gavin@saucelabs.com>
2016-05-21 13:26:09 -0700

    show the changes in the commit as well

--- public/js/app.js
+++ public/js/app.js
@@ -46,10 +46,14 @@ const getConfig = memoize(function () {
       return Promise.all(projects.map((project) => {
         return tagsForProject(project).then(tags => {
           project.tags = tags;
-          project.commits_for_sha = {};
-          return getCommit(project, tags[0]).then(commits => {
-            project.commits_for_sha[tags[0]] = commits[0]
-          });
+          project.commits = [];
+          if (tags.length > 1) {
+            return compareSha(project, tags[1], tags[0])
+              .then(commits => project.commits = commits);
+          } else {
+            return getCommit(project, tags[0])
+              .then(commit => project.commits = [commit]);
+          }
         });
       })).then(function() { return projects });
     });
@@ -64,7 +68,34 @@ const tagsForProject = memoize(function (project) {
 
 const getCommit = memoize(function (project, sha) {
   return fetch('https://api.github.com/repos/' + project.repo + '/commits?sha=' + sha, fetchOptions)
-    .then(response => response.json());
+    .then(response => response.json())
+    .then(commits => { return processCommit(commits[0]); });
+});
+
+const processCommit = function (commit) {
+  return {
+    message: commit.commit.message.replace(/Merge pull request #\d+ from \w+\/[^ ]+/, ''),
+    sha: commit.sha,
+    date: commit.commit.author.date,
+    author: {
+      name: commit.commit.author.name,
+      email: commit.commit.author.email,
+      avatar_url: commit.author.avatar_url,
+      login: commit.author.login
+    },
+    committer: {
+      name: commit.commit.committer.name,
+      email: commit.commit.committer.email,
+      avatar_url: commit.committer.avatar_url,
+      login: commit.committer.login
+    }
+  };
+}
+
+const compareSha = memoize(function (project, sha1, sha2) {
+  return fetch(`https://api.github.com/repos/${project.repo}/compare/${sha1}...${sha2}`, fetchOptions)
+    .then(response => response.json())
+    .then(json => { return json.commits.map(commit => processCommit(commit)) });
 });
 
 class App extends React.Component {
@@ -77,7 +108,12 @@ class App extends React.Component {
   componentDidMount() {
     getConfig()
       .then(config => this.setState({ projects: config }))
-      .catch(error => this.setState({ error: 'error fetching config:' + error }));
+      /*
+      .catch(error => {
+        console.error(error); // eslint-disable-line
+        this.setState({ error: 'error fetching config:' + error })
+      });
+      */
   }
 
   render() {
@@ -96,33 +132,46 @@ class App extends React.Component {
 
 }
 
+class Commit extends React.Component {
+  static propTypes = {
+    commit: React.PropTypes.object.isRequired
+  };
+  render() {
+    return (
+      <div className="commit">
+        <span className="message">{this.props.commit.message}</span>
+        <span className="author">{this.props.commit.author.name}</span>
+        <span className="sha">[{this.props.commit.sha.slice(0,6)}]</span>
+      </div>
+    );
+  }
+}
+
 class Project extends React.Component {
   static propTypes = {
     project: React.PropTypes.object.isRequired,
-    commit: React.PropTypes.object
+    commits: React.PropTypes.array
   };
   render() {
-    const commit = this.props.commit;
+    const commits = this.props.commits;
     const project = this.props.project;
 
-    if (!commit) { return <div />; }
+    if (!commits) { return <div />; }
 
     return (
       <div className="card">
         <div className="header">
           <div className="info">
             <div className="repo">{project.repo.split('/')[1]}</div>
-            <span className="ago"><SmartTimeAgo value={new Date(commit.commit.committer.date)} /></span>
+            <span className="ago"><SmartTimeAgo value={new Date(commits[0].date)} /></span>
             <span> by </span>
-            <span className="author">{commit.commit.author.name}</span>
+            <span className="author">{commits[0].author.name}</span>
           </div>
-          <UserAvatar size='60' name={commit.commit.author.name} src={commit.committer.avatar_url} className="avatar" />
+          <UserAvatar size='60' name={commits[0].author.name} src={commits[0].author.avatar_url} className="avatar" />
         </div>
         <div className="body">
-          {commit.commit.message}
-        </div>
-        <div className="footer">
-          Card footer with action buttons
+          <h2>{project.tags.slice(0,2).reverse().join('...')}</h2>
+          {commits.map((commit) => <Commit key={commit.sha} commit={commit} />)}
         </div>
       </div>
     );
@@ -141,11 +190,11 @@ class Projects extends React.Component {
         <GithubCorner href="https://github.com/halkeye/release-dashboard" />
         <div className="projectsContainer">{
           this.props.projects.map(project => {
-            if (!project.commits_for_sha) { return <div />; }
+            if (!project.commits || !project.commits.length) { return <div />; }
             return <Project
               key={project.repo}
               project={project}
-              commit={project.commits_for_sha[project.tags[0]]}
+              commits={project.commits}
             />;
           })
         }</div>
