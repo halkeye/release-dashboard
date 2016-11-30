c232eae3611d44496dc153c210ecf2a60bb7bee3
Gavin Mogan <gavin@saucelabs.com>
2016-11-30 12:09:30 -0800

    wrong tag date was being passed to colorize functions

--- src/js/components/Project.js
+++ src/js/components/Project.js
@@ -10,16 +10,7 @@ import Progress from 'essence-progress';
 import TagHeader from './TagHeader.js';
 import Commit from './Commit.js';
 
-function projectStaleness(lastUpDate, target = 1) {
-  let daysAgo = (Date.now() - lastUpDate.getTime()) / 1000 / 60 / 60 / 24;
-  // magic coefficient that shapes the staleness curve
-  // experimentally determined!!!
-  let stalenessCoeff = 1 / (2.4 * Math.pow(target, 0.7));
-  // generate a 1/(sqrt(x)+1)-type curve with some fiddly awesomeness
-  let staleness = (Math.pow((stalenessCoeff * daysAgo) + 1, -1)) /
-                  ((stalenessCoeff * Math.sqrt(daysAgo)) + 1);
-  return staleness;
-}
+import projectStaleness from '../projectStaleness.js';
 
 export default class Project extends React.Component {
   static propTypes = {
@@ -36,7 +27,7 @@ export default class Project extends React.Component {
     // once per day
     if (commits && useTarget) {
       const depTargetInt = this.props.project.deployTargetInterval || 1;
-      const staleness = projectStaleness(new Date(project.tags[0].tagger.date), depTargetInt);
+      const staleness = projectStaleness(new Date(project.tags[1].tagger.date), depTargetInt);
       cardStyle.backgroundColor = numberToColorHsl(staleness);
     }
 
---
+++ src/js/projectStaleness.js
@@ -0,0 +1,12 @@
+
+export default function projectStaleness(lastUpDate, target = 1) {
+  let daysAgo = (Date.now() - lastUpDate.getTime()) / 1000 / 60 / 60 / 24;
+  // magic coefficient that shapes the staleness curve
+  // experimentally determined!!!
+  let stalenessCoeff = 1 / (2.4 * Math.pow(target, 0.7));
+  // generate a 1/(sqrt(x)+1)-type curve with some fiddly awesomeness
+  let staleness = (Math.pow((stalenessCoeff * daysAgo) + 1, -1)) /
+                  ((stalenessCoeff * Math.sqrt(daysAgo)) + 1);
+  return staleness;
+}
+
